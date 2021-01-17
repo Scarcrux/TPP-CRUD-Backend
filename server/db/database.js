9e0a9c7af58d6c06b37977464557b8f011b465e6
Scarcrux <reg@scarcrux.com>
2021-01-17 11:53:50 -0500

    Deploy to Heroku - #9
    completes #9

--- server/db/database.js
+++ server/db/database.js
@@ -12,10 +12,21 @@ const name = process.env.DATABASE_NAME || pkg.name;
 const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${pkg.name}`;
 
 console.log(chalk.yellow(`Opening database connection to ${connectionString}`));
-
+let db;
+if (process.env.HEROKU_POSTGRESQL_BROWN_URL) {
+  // the application is executed on Heroku ... use the postgres         database
+db = new Sequelize(process.env.HEROKU_POSTGRESQL_BROWN_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  port: 5432,
+  host: "https://tpp-crud-backend.herokuapp.com/",
+  logging: false //false
+});
+} else {
 // create the database instance that can be used in other database files
-const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
+db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
   logging: false // so we don't see all the SQL queries getting made
 })
+}
 
 module.exports = db
