e46f273884031d078defceb7384a3e3e65f92ef4
Scarcrux <reg@scarcrux.com>
2021-01-17 06:18:49 -0500

    Post campuses Express routes

--- server/db/index.js
+++ server/db/index.js
@@ -5,6 +5,7 @@ const db = require('./database')
 const Campus = require('./Campus');
 const Student = require('./Student');
 
+Campus.belongsToMany(Student, {through: 'campus_student'});
 Student.belongsTo(Campus);
 
 module.exports = {
